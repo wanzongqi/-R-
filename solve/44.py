79e901b6b5bacd25999f50b4acab38d6c9ed6b9d
strA <201600090059@mail.sdu.edu.cn>
2018-03-09 20:17:02 +0800

    first

---
+++ solve/44.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@author: strA
+
+ProjectEuler-problem-44
+"""
+
+"""
+先写个简单的
+"""
+import math
+def check(n):
+    if int((1+math.sqrt(1+24*n))/6)==(1+math.sqrt(1+24*n))/6:
+        return True
+    else:
+        return False
+    
+def Pentagon(n):
+    return (n*(3*n-1))//2
+
+i = 1
+flag = True
+while flag:
+    j = 1
+    while j<i:
+        diff = Pentagon(i)-Pentagon(j)
+        sum = Pentagon(i)+Pentagon(j)
+        if check(diff) and check(sum):
+            flag = True
+            print(diff)
+        j +=1
+    i += 1
+
+    
